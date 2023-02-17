9a33e7122ae8b5f5a6f8e374b170137d4aa674f0
WalkerSyed581 <sajeelh581@gmail.com>
2023-02-17 12:48:47 +0500

    added some headers and nav

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,14 @@
+import Head from "next/head";
+
 const about = () => {
-  return;
-  <>
-    <Head>
-      <title>WebDev Newz | About</title>
-    </Head>
-    <h1>About</h1>
-  </>;
+  return (
+    <>
+      <Head>
+        <title>WebDev Newz | About</title>
+      </Head>
+      <h1>About</h1>
+    </>
+  );
 };
 
 export default about;
